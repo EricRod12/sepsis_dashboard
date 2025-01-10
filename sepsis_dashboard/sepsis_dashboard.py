e878ad4ba33eba62b94e0c2ad9380ce99e84befc
Eric Rodriguez <141654114+EricRod12@users.noreply.github.com>
2025-01-09 22:26:25 -0500

    Update sepsis_dashboard.py

--- sepsis_dashboard/sepsis_dashboard.py
+++ sepsis_dashboard/sepsis_dashboard.py
@@ -9,7 +9,7 @@ from sklearn.preprocessing import StandardScaler
 # 1. Load Model and Data
 # --------------------------
 
-model = joblib.load(r"stacking_model.pkl")  # Replace with your actual model path
+model = joblib.load("stacking_model.pkl")  # Replace with your actual model path
 
 file_path = r"sepsis.csv"
 df = pd.read_csv(file_path)
